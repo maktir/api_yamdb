d888600191084100ec5dcd55f5c4d4937935de3d
Arman Aloian <armanaloian@yandex.ru>
2021-07-13 23:57:51 +0300

    Review_Comment_models_V1

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Review(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата отзыва', auto_now_add=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='review'
+    )
+    title = models.ForeignKey(Title, on_delete=models.CASCADE, related_name='review'
+    )
+    score = models.IntegerField()
+    
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='comments'
+    )
+    review  = models.ForeignKey(
+        Review, on_delete=models.CASCADE, related_name='comments'
+    )
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата комментария', auto_now_add=True, 
+    )
+
+    def __str__(self):
+        return self.text
