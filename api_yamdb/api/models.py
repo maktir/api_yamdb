c12729c9b7ea69db5f918cf8f7b21d9bf4710c97
garuan2 <garuan2@gmail.com>
2021-07-13 19:42:23 +0300

    Models Genre Category Title create

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Genre(models.Model):
+    name = models.TextField()
+    slug = models.SlugField(unique=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Category(models.Model):
+    name = models.TextField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Title(models.Model):
+    name = models.TextField()
+    year = models.DateTimeField(
+        "Дата издания",
+        blank=True,
+        null=True
+    )
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 related_name='titles',
+                                 blank=True,
+                                 null=True,
+                                 verbose_name='Категория',
+                                 help_text='Тип произведения')
+    genre = models.ForeignKey(Genre,
+                              on_delete=models.SET_NULL,
+                              related_name='titles',
+                              blank=True,
+                              null=True,
+                              verbose_name='Жанр',
+                              help_text='Жанр произведения')
+
+    def __str__(self):
+        return self.name
