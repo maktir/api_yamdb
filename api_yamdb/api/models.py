a8fc27591b7b884234bc88f5f97d3c9c2a236a9d
garuan2 <garuan2@gmail.com>
2021-07-14 08:40:19 +0300

    Corrected Models for Genre_Category_Title

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,19 +1,17 @@
-from django.contrib.auth import get_user_model
 from django.db import models
-
-User = get_user_model()
+import datetime as dt
 
 
 class Genre(models.Model):
-    name = models.TextField()
-    slug = models.SlugField(unique=False)
+    name = models.CharField(max_length=200, verbose_name='Название Жанра')
+    slug = models.SlugField()
 
     def __str__(self):
         return f'{self.name}'
 
 
 class Category(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=200, verbose_name='Название Категории')
     slug = models.SlugField(unique=True)
 
     def __str__(self):
@@ -22,18 +20,30 @@ class Category(models.Model):
 
 class Title(models.Model):
     name = models.TextField()
-    year = models.DateTimeField(
-        "Дата издания",
-        blank=True,
-        null=True
-    )
+    year = models.IntegerField(default=lambda: dt.date.today().year,
+                               verbose_name='Категория',
+                               blank=True,
+                               null=True
+                               )
+    rating = models.DecimalField(verbose_name='Рейтинг',
+                                 max_digits=2,
+                                 decimal_places=2,
+                                 blank=True,
+                                 null=True
+                                 )
+    description = models.CharField(max_length=200,
+                                   verbose_name='Описание',
+                                   blank=True,
+                                   null=True
+                                   )
     category = models.ForeignKey(Category,
                                  on_delete=models.SET_NULL,
                                  related_name='titles',
                                  blank=True,
                                  null=True,
                                  verbose_name='Категория',
-                                 help_text='Тип произведения')
+                                 help_text='Тип произведения'
+                                 )
     genre = models.ForeignKey(Genre,
                               on_delete=models.SET_NULL,
                               related_name='titles',
