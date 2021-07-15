b756f6a9462ac2593e9895280e1c1709cacf7f0d
Arman Aloian <armanaloian@yandex.ru>
2021-07-15 07:03:14 +0300

    Review_Comment_models_V2

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,8 +1,5 @@
-from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-
-User = get_user_model()
 
 
 class Review(models.Model):
