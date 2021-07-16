45df671a8eeb7fa7f8650c0cf0576eb6f5baa5af
garuan2 <garuan2@gmail.com>
2021-07-16 09:20:10 +0300

    Title_Genre_Category_views

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -25,8 +25,8 @@ router_api.register(
 )
 
 urlpatterns = [
-    path('', include(router_api.urls)),
-    path('token/', TokenObtainPairView.as_view(),
+    path('v1', include(router_api.urls)),
+    path('v1/token/', TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('v1/token/refresh/', TokenRefreshView.as_view(),
          name='token_refresh'),
