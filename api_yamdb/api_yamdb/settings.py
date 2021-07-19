97b04372b84887493c2d399041df6fed9e7f245a
garuan2 <garuan2@gmail.com>
2021-07-19 21:25:55 +0300

    Partof tests passed

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -140,7 +140,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 10
 }
 
 SIMPLE_JWT = {
