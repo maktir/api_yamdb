d88cf54e9f2be97bb91b1f3590801c8918ee4eb5
garuan2 <garuan2@gmail.com>
2021-07-20 08:04:40 +0300

    Updated

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -18,10 +18,10 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 class IsAdminOrRO(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
+        if request.user.is_superuser or request.method in permissions.SAFE_METHODS:
             return True
         else:
-            return request.user.is_staff
+            return request.user.is_superuser
 
 
 
