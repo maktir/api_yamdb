8f5f79e7ea179c2ce8f52b8e369ecb1c953b4465
unknown <emb3231323@gmail.com>
2021-07-24 14:41:23 +0300

    jwt fix 2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
     'django_filters',
     'users',
     'api',
